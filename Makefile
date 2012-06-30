b8d55971f0922c72c3d09b86340f977baa26241c
southkyle <vip_liming@126.com>
2012-06-30 16:52:05 +0800

    ignore core *.out.

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 
 memcpy:
 	g++ memcpy.cc -o memcpy
+quicksort:
+	g++ quicksort.cc -o quicksort
 clean:
 	rm -f *.o
 	rm memcpy
+	rm quicksort
--- memcpy.cc
+++ memcpy.cc
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cassert>
 #include <cstring>
-
+#include <cstdint>
 using namespace std;
 // my version
 void* memcpy(void* dst, void* src, size_t n) {
@@ -13,7 +13,9 @@ void* memcpy(void* dst, void* src, size_t n) {
   return dst;
 }
 
-void* memcpy2(void* dst, void* src, size_t count) {
+// standard version
+// Mention that you can use allignment to speed up may be good
+void* memcpy2(void* dst, const void* src, size_t count) {
   assert(dst != NULL);
   void* ret = dst;
   while(count--) {
@@ -24,6 +26,25 @@ void* memcpy2(void* dst, void* src, size_t count) {
   return ret;
 }
 
+// Speed up
+void* memcpy3(void* dst, const void* src, size_t count) {
+  assert(dst != NULL);
+  int32_t* int_dst = (int32_t*)dst;
+  int32_t* int_src = (int32_t*)src;
+  // 如果是对其的那么可以快速处理
+  if (int_dst is align && int_src is align) {
+    while (count >= 4) {
+      *int_dst++ = *int_src++;
+      count -= 4;
+    }
+  }
+  char* ptr_dst = (char*)int_dst;
+  char* ptr_src = (char*)ptr_src;
+  while (count != 0) {
+    *pstr_dst++ = ptr_src++;
+  }
+  return dst;
+}
 int main() {
   char from[1024] = {"Apple is big"};
   char dest[1024];
