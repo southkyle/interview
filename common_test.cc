5e2b1cf82fda05a33699138bf94ceef0da51d21e
southkyle <vip_liming@126.com>
2012-07-23 22:27:57 +0800

    nothing

--- common_test.cc
+++ common_test.cc
@@ -2,7 +2,7 @@
 
 int main(int argc, char *argv[]) {
   ASSERT_EQ(1, 1);
-//  int array[] = {1, 2, 3};
-//  ASSERT_EQ(arraysize(array), 3);
+  int array[] = {1, 2, 3};
+  ASSERT_EQ(arraysize(array), 3);
   return 0;
 }
